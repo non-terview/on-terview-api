cd3793779a1de38d7f0f01158904c49b682978b5
warmoil <69149722+warmoil@users.noreply.github.com>
2021-09-21 15:32:48 +0900

    보드를 하나로 통일했습니다 . #21 (#35)
    이제는 타입으로 자유게시판인지 회사게시판인지 구분 ,
    그후 카테고리로 자게 , 회사게 에서 또 카테고리로 구분(이전에는 타입으로 구분하려 했던작업임  )
    
    Co-authored-by: warmoil <jipmj12@gmail.com>

--- src/main/java/site/askephoenix/restapi/board/controller/BoardController.java
+++ src/main/java/site/askephoenix/restapi/board/controller/BoardController.java
@@ -2,20 +2,13 @@ package site.askephoenix.restapi.board.controller;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.askephoenix.restapi.annotation.LoginUser;
-import site.askephoenix.restapi.board.model.CompanyBoardInfo;
-import site.askephoenix.restapi.board.repository.CompanyBoardRepository;
 import site.askephoenix.restapi.board.service.BoardService;
-import site.askephoenix.restapi.page.vo.PageMaker;
-import site.askephoenix.restapi.page.vo.PageVO;
 import site.askephoenix.restapi.user.model.UserInfo;
 
 import java.util.HashMap;
--- src/main/java/site/askephoenix/restapi/board/service/Impl/BoardServiceImpl.java
+++ src/main/java/site/askephoenix/restapi/board/service/Impl/BoardServiceImpl.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import site.askephoenix.restapi.board.model.CompanyBoardInfo;
-import site.askephoenix.restapi.board.repository.CompanyBoardRepository;
+import site.askephoenix.restapi.board.model.BoardInfo;
+import site.askephoenix.restapi.board.repository.BoardRepository;
 import site.askephoenix.restapi.board.service.BoardService;
 import site.askephoenix.restapi.page.vo.PageMaker;
 import site.askephoenix.restapi.page.vo.PageVO;
@@ -17,14 +17,14 @@ import java.util.HashMap;
 @Service
 @RequiredArgsConstructor
 public class BoardServiceImpl implements BoardService {
-    private final CompanyBoardRepository repo;
+    private final BoardRepository repo;
 
     @Override
     public HashMap<String,Object> getBoardPage( int pageNum ){
         PageVO vo = new PageVO();
         vo.setPage(pageNum);
         Pageable pageable = vo.makePageable(0, "seq");
-        Page<CompanyBoardInfo> result = repo.findAllByCompanyNameContainingOrderBySeq("번째", pageable);
+        Page<BoardInfo> result = repo.findAllByCompanyNameContainingOrderBySeq("번째", pageable);
         return Maps.newHashMap(ImmutableMap.of("pageInfo", new PageMaker(result)));
     }
 }
