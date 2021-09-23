4ac12fc23ef206ac4947436d33e86b08a66fe6ab
askePhoenix <aistarteverything@gmail.com>
2021-09-23 22:07:06 +0900

    Evaluation Controller, Dto 작업 #33

--- src/main/java/site/askephoenix/restapi/evaluation/dto/EvaluationInfoDto.java
+++ src/main/java/site/askephoenix/restapi/evaluation/dto/EvaluationInfoDto.java
@@ -3,7 +3,14 @@ package site.askephoenix.restapi.evaluation.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
+// frontend api
 @Getter
 @Setter
 public class EvaluationInfoDto {
+    private int gradations;
+    private String title;
+    private List<EvaluationDetailInfoDto> details;
+
 }
