46eb421c3ac6bcd9b8d65d5fa45f5e55fa7c2415
askePhoenix <73146032+askePhoenix@users.noreply.github.com>
2021-09-25 20:03:09 +0900

    status Info, Dto 추가 #27

--- src/main/java/site/askephoenix/restapi/status/dto/StatusInfoDto.java
+++ src/main/java/site/askephoenix/restapi/status/dto/StatusInfoDto.java
@@ -1,4 +1,16 @@
 package site.askephoenix.restapi.status.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import site.askephoenix.restapi.board.dto.BoardDto;
+import site.askephoenix.restapi.user.dto.UserInfoDto;
+
+@Getter
+@Setter
 public class StatusInfoDto {
+    private Long id;
+    private UserInfoDto userInfoDto;
+    private BoardDto boardDto;
+    private String state;
+    private boolean isDeleted;
 }
--- src/main/java/site/askephoenix/restapi/status/model/StatusInfo.java
+++ src/main/java/site/askephoenix/restapi/status/model/StatusInfo.java
@@ -1,11 +1,16 @@
 package site.askephoenix.restapi.status.model;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import site.askephoenix.restapi.board.model.BoardInfo;
 import site.askephoenix.restapi.user.model.UserInfo;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
@@ -21,5 +26,33 @@ public class StatusInfo {
     @JoinColumn(name = "userinfo")
     private UserInfo userInfo;
 
+    @JoinColumn(name = "board")
+    @ManyToOne
+    private BoardInfo boardInfo;
+
+    @Column(name = "state_value")
+    private String state;
+
+    @Column(name = "create_date")
+    @CreationTimestamp
+    private LocalDate createDate;
+
+    @Column(name = "update_date")
+    @UpdateTimestamp
+    private LocalDate updateDate;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted;
+
+
+    @Builder
+    public StatusInfo(
+            Long id, UserInfo userInfo,
+            boolean isDeleted
+    ) {
+        this.id = id;
+        this.userInfo = userInfo;
+        this.isDeleted = isDeleted;
+    }
 
 }
