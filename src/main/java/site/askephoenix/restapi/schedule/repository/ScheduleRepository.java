06e7b8eaac183688978834e21855b23fc4b7bcbf
warmoil <jipmj12@naver.com>
2021-09-27 23:24:09 +0900

    레파지토리 작성  #26

--- src/main/java/site/askephoenix/restapi/schedule/repository/ScheduleRepository.java
+++ src/main/java/site/askephoenix/restapi/schedule/repository/ScheduleRepository.java
@@ -1,5 +1,7 @@
 package site.askephoenix.restapi.schedule.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import site.askephoenix.restapi.schedule.model.ScheduleInfo;
 import site.askephoenix.restapi.user.model.UserInfo;
@@ -7,5 +9,6 @@ import site.askephoenix.restapi.user.model.UserInfo;
 public interface ScheduleRepository extends CrudRepository<ScheduleInfo, UserInfo> {
 
     //
+    Page<ScheduleInfo> findAllByUserInfo(UserInfo userInfo, Pageable pageable);
 
 }
