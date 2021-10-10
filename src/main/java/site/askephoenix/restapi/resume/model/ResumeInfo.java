9fe798a0d42f6c85c244311c5040f371c340c29f
askePhoenix <73146032+askePhoenix@users.noreply.github.com>
2021-10-10 16:15:09 +0900

    Update ResumeInfo.java
    - 불필요 Import 제거하였습니다.

--- src/main/java/site/askephoenix/restapi/resume/model/ResumeInfo.java
+++ src/main/java/site/askephoenix/restapi/resume/model/ResumeInfo.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import site.askephoenix.restapi.annotation.LoginUser;
 import site.askephoenix.restapi.user.model.UserInfo;
 
 import javax.persistence.*;
