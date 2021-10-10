809df0e88caace7990acf95880b97fe96cff43fd
askePhoenix <73146032+askePhoenix@users.noreply.github.com>
2021-10-10 19:53:20 +0900

    Controller Test [docs] #32
    - 5-2. 로그인 사용자가 작성한 모의시험 리스트 가져오기 (회사)

--- src/test/java/site/askephoenix/restapi/company_test/controller/CompanyTestsControllerTest.java
+++ src/test/java/site/askephoenix/restapi/company_test/controller/CompanyTestsControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -20,10 +21,11 @@ import site.askephoenix.restapi.company_test.model.CompanyTestsInfo;
 import site.askephoenix.restapi.company_test.service.CompanyTestsResultService;
 import site.askephoenix.restapi.company_test.service.CompanyTestsService;
 import site.askephoenix.restapi.company_test.service.TestsListService;
+import site.askephoenix.restapi.user.dto.UserResultDto;
 import site.askephoenix.restapi.user.model.UserInfo;
 import site.askephoenix.restapi.user.service.UserService;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -31,7 +33,6 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
@@ -62,6 +63,9 @@ class CompanyTestsControllerTest {
     @Mock
     UserInfo userInfo;
 
+    @Mock
+    UserResultDto userDto;
+
     @BeforeEach
     void setup(WebApplicationContext webApplicationContext,
                RestDocumentationContextProvider restDocumentation) {
@@ -69,6 +73,11 @@ class CompanyTestsControllerTest {
                 .apply(springSecurity())
                 .apply(documentationConfiguration(restDocumentation))
                 .build();
+        this.userInfo = UserInfo.builder()
+                .id(1L).auth("ROLE_USER").email("tester")
+                .password(new BCryptPasswordEncoder().encode("1234test"))
+                .type("").name("김주윤").build();
+        this.userDto = new UserResultDto(userInfo);
     }
 
 
@@ -81,30 +90,25 @@ class CompanyTestsControllerTest {
     @Test
     @DisplayName(value = "로그인 사용자가 작성한 모의시험 리스트 가져오기")
     void getCompanyTests() throws Exception {
-        UserInfo userInfo = UserInfo.builder()
-                .auth("ROLE_USER")
-                .email("tester")
-                .build();
         CompanyTestsDto dto1 = new CompanyTestsDto(
                 CompanyTestsInfo.builder()
                         .id(1L)
                         .writer(userInfo)
-                        .createDate(LocalDate.now())
-                        .updateDate(LocalDate.now())
+                        .createDate(LocalDateTime.now())
+                        .updateDate(LocalDateTime.now())
                         .build()
         );
         CompanyTestsDto dto2 = new CompanyTestsDto(
                 CompanyTestsInfo.builder()
                         .id(2L)
                         .writer(userInfo)
-                        .createDate(LocalDate.now())
-                        .updateDate(LocalDate.now())
+                        .createDate(LocalDateTime.now())
+                        .updateDate(LocalDateTime.now())
                         .build()
         );
         List<CompanyTestsDto> dtoList = List.of(dto1, dto2);
 
         given(service.readUserTests(any(UserInfo.class))).willReturn(dtoList);
-
 
 
         ResultActions perform = this.mvc.perform(
@@ -114,16 +118,34 @@ class CompanyTestsControllerTest {
         );
         perform.andExpect(status().isOk())
                 .andDo(print())
-                .andDo(document("company_test-get",
+                .andDo(document("company_test-get-user",
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()),
                         pathParameters(
                         ),
                         responseFields(
-                                fieldWithPath("[]").description("정보 map"),
-                                fieldWithPath("[].id").description("평가 기준표 식별자"),
-                                fieldWithPath("[].writer").description("평가 기준표 식별자"),
-                                fieldWithPath("[].createDate").description("테스트 성공 여부")
+                                fieldWithPath("[]").description("모의시험 배열"),
+                                fieldWithPath("[].id").description("모의시험 식별번호"),
+                                fieldWithPath("[].writer").description("모의시험 작성자"),
+                                fieldWithPath("[].writer.id").description("작성자 식별번호"),
+                                fieldWithPath("[].writer.name").description("작성자 성명"),
+                                fieldWithPath("[].testsListDto").description("테스트 항목배열 (빈값)"),
+                                fieldWithPath("[].createDate[]").description("작성 년/월/일 시/분/초 총값"),
+                                fieldWithPath("[].createDate[0]").description("년"),
+                                fieldWithPath("[].createDate[1]").description("월"),
+                                fieldWithPath("[].createDate[2]").description("일"),
+                                fieldWithPath("[].createDate[3]").description("시"),
+                                fieldWithPath("[].createDate[4]").description("분"),
+                                fieldWithPath("[].createDate[5]").description("초"),
+                                fieldWithPath("[].createDate[6]").description("시간 총 값"),
+                                fieldWithPath("[].updateDate[]").description("작성 년/월/일 시/분/초 총값"),
+                                fieldWithPath("[].updateDate[0]").description("년"),
+                                fieldWithPath("[].updateDate[1]").description("월"),
+                                fieldWithPath("[].updateDate[2]").description("일"),
+                                fieldWithPath("[].updateDate[3]").description("시"),
+                                fieldWithPath("[].updateDate[4]").description("분"),
+                                fieldWithPath("[].updateDate[5]").description("초"),
+                                fieldWithPath("[].updateDate[6]").description("시간 총 값")
                         )
                 ));
     }
